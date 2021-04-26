010a6ceaa3c7def89d7ab1d267c63a28e427e807
Peter Lee <mlleepeter@gmail.com>
2021-04-26 14:39:58 +0800

    remove kartographer, add multimaps

--- extensions/Kartographer/.phan/stubs/namespaces.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * namespace constant defined in extension.json of JsonConfig extension
- * Can be removed, when JsonConfig get a similar stub to pass phan
- */
-define( 'NS_DATA', 486 );
-define( 'NS_DATA', 487 );
--- extensions/Kartographer/README.md
+++
@@ -1,2 +0,0 @@
-Kartographer is a MediaWiki extension that adds map capability
-See  https://www.mediawiki.org/wiki/Extension:Kartographer
